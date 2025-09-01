e64bc3cd0e50053b0af340dcc3f0799fcafceaeb
Mark McCahill <mark.mccahill@duke.edu>
2025-09-01 16:36:25 -0400

    cleanup, and get the container hostname
    we can use the container hsotname to track sessions
    so grab it for later use in logging.
    
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- R/app.R
+++ R/app.R
@@ -1,6 +1,12 @@
 # app.R
 library(stringi)
 
+# get the container hostname we are running inside
+get_hostname <- function() {
+  Sys.info()[["nodename"]]
+}
+
+
 #' Capture Selected Text
 #'
 #' An RStudio addin that captures the currently selected text in the editor
@@ -123,7 +129,7 @@ ui <- fluidPage(
   
   # Student answer input
   textAreaInput("student_answer", "Your answer", value = capture_selection(), rows = 6, cols = 80),
-
+  
   # Submit button
   actionButton("reset", "Copy selection to answer"),
   actionButton("submit", "Get feedback"),
@@ -136,57 +142,6 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output, session) {
-  
-  # Define the feedback function 
-  get_assignmnent_feedback <- function(homework_name, student_answer) {
-    # Load required packages
-    if (!require("httr")) install.packages("httr")
-    if (!require("jsonlite")) install.packages("jsonlite")
-    library(httr)
-    library(jsonlite)
-    
-    body_data <- list(
-      data = c(homework_name, student_answer)
-    )
-    json_body <- toJSON(body_data, auto_unbox = TRUE)
-    post_url <- "https://chatbot-az-00.oit.duke.edu:5443/gradio_api/call/predict/"  # Replace with actual URL
-    post_result <- tryCatch({
-      # Attempt the POST request
-      response <- POST(
-        url = post_url,
-        body = json_body,
-        content_type("application/json"),
-        accept("application/json")
-      )
-      if (status_code(response) != 200) {
-        stop("POST request failed with status code: ", status_code(response))
-      }
-      response_content <- content(response, "text")
-      result <- fromJSON(response_content)
-      list(event_id = result$event_id)
-    }, error = function(e) {
-      list(error = "POST request or parsing error", message = e$message)
-    })
-    if (!is.null(post_result$error)) {
-      return(post_result)
-    }
-    event_id <- post_result$event_id
-    
-    get_url <- paste0("https://chatbot-az-00.oit.duke.edu:5443/gradio_api/call/predict/", event_id)
-    get_result <- tryCatch({
-      response <- GET(url = get_url)
-      if (status_code(response) != 200) {
-        stop("GET request failed with status code: ", status_code(response))
-      }
-      result <- content(response, "text")
-      result
-    }, error = function(e) {
-      return( list(error = "GET request or parsing error", message = e$message) )
-    })
-    
-    return(get_result)
-  }
-  #### end of get_assignmnent_feedback function
   
   # strip the superfluous garbage off the LLM feedback
   clean_result <- function(dirty_input) {
@@ -215,6 +170,10 @@ server <- function(input, output, session) {
   feedback_value <- reactiveVal("")
   
   observeEvent(input$submit, {
+    # get the name of the container (host) we are running in
+    hidden_hostname_value <- Sys.info()[["nodename"]]
+    # print(hidden_hostname_value)
+    
     feedback_value("")
     homework_name <- input$homework_name
     student_answer <- input$student_answer
